26f78e79ae66a33ad82810cec754d55a918b1782
Mohd Sajeed <67324375+mohd-sajeed@users.noreply.github.com>
2024-05-15 11:08:03 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 145569
+let accountEmail = "sajid@gmail.com"
+var accountPassword = "Sajid@143"
+ accountCity = "Hyderabad"
+ 
+// accountId = 2 // not allowed
+accountEmail =" googfy@gmail.com"
+accountPassword="Samosa12"
+accountCity ="Bangalore"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+// prefer not to use Var
+// because of issue in block scope and functional scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all JS code as newer version
+
+//alert( 4 + 4)  we are using nodejs, not browser
+
+console.log(4 + 4);
+console.log("Sajid");
+
+let username = "Sajid";
+let age = 18;
+let isLogged = true || false;
+let state;
+
+// number => 2 to power 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// undefined =>
+// null => Standalone value
+// Symbols => Unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // Object
+
