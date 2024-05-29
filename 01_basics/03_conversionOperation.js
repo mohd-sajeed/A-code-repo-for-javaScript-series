6f8fbfc2295abf2cdd7eb4259d34ceef688093f7
Mohd Sajeed <67324375+mohd-sajeed@users.noreply.github.com>
2024-05-29 11:17:20 +0000

    operator and comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 44
  let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+
+let str1 = "Sajid"
+let str2 = " Wajid"
+ 
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log("1" + 2 + 2) 
+// console.log(1+ 2 + "2")
+
+// console.log(+true);
+// console.log(+ "");
+
+let gameCounter = 100
+gameCounter++
+// ++gameCounter
+// console.log(+true);
+// console.log(gameCounter);
+
+// console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log("2" === 1);
