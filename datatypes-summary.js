c5b6aa20271e98810f955f420a076854429bf588
Mohd Sajeed <67324375+mohd-sajeed@users.noreply.github.com>
2024-05-29 12:52:41 +0000

    change in location of the file

---
+++ datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitives
+
+// 7 types : String, Number, Boolean,null, undefined,symbol, BigInt 
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+let score = 100
+let scoreValue = 100.3
+
+const isLogged = false
+const outsideTemp = null
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+const bigNumber = 15384679254562n
+
+const heroes = ["shaktimaan", "anpanman", "doraman"]
+
+let myObj = {
+    name : "Sajid",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof anotherId);
